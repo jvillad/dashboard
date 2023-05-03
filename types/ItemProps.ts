393d3b2023374f84f926f6a7cfc9da03416fdab1
jvillad <jyvillad@gmail.com>
2023-05-03 21:46:56 +1000

    more props!

--- types/ItemProps.ts
+++ types/ItemProps.ts
@@ -7,3 +7,33 @@ export type ItemProps = {
   category: string;
   special: boolean;
 };
+
+export interface Product {
+  product: {
+    id: string;
+    name: string;
+    description: string | null;
+    price: string;
+    stock: string;
+    category: string;
+    special: boolean;
+  };
+}
+
+export interface EditItemProps {
+  product: {
+    id: string;
+    name: string;
+    description: string;
+    price: string;
+    stock: string;
+    category: string;
+    special: boolean;
+  };
+}
+
+export interface ProductId {
+  params: {
+    productId: string;
+  };
+}
