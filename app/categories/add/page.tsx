bc44eec65b49e2d4aa7f1d4b45e2ce049121c735
jvillad <jyvillad@gmail.com>
2023-05-05 18:21:54 +1000

    add categores component

---
+++ app/categories/add/page.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout';
+import AddCategory from '@/components/category/AddCategory';
+
+const page = () => {
+  return (
+    <Layout>
+      <AddCategory />
+    </Layout>
+  );
+};
+
+export default page;
