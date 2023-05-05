71ff4e1aa9196728cda0b8a7c8d22599b7bf8d52
jvillad <jyvillad@gmail.com>
2023-05-05 18:13:17 +1000

    CRUD for category table

---
+++ pages/api/category/createCategory.ts
@@ -0,0 +1,22 @@
+import prisma from '@/lib/prisma';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function createItem(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { name, description } = req.body;
+  if (req.method === 'POST') {
+    try {
+      const data = await prisma.category.create({
+        data: {
+          name,
+          description,
+        },
+      });
+      return res.status(200).json('post');
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+}
