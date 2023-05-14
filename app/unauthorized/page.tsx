10f69c6104555f1eeda6dc27ed61af36e12dcc0d
jvillad <jyvillad@gmail.com>
2023-05-14 22:26:30 +1000

    page to reroute all not authorized user

---
+++ app/unauthorized/page.tsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <p className=" font-bold ">
+        Access Denied. You are not authorized to perform this action or access
+        this resource. Please contact your administrator for assistance.
+      </p>
+    </div>
+  );
+};
+export default page;
